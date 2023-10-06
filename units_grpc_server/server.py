2fe8b18bd0d0bd970edb626a344a328b0f56cf41
Ken Wu <kenwu1009us@gmail.com>
2023-10-05 22:45:57 -0700

    add grpc server

--- units_grpc_server/server.py
+++ units_grpc_server/server.py
@@ -11,22 +11,48 @@ from units_proto import units_pb2
 from units_proto import units_pb2_grpc
 from units_proto_extensions.extensions import convert_units
 
+class ConversionServer():
+    def __init__(self):
+        self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        units_pb2_grpc.add_UnitConversionServiceServicer_to_server(
+            ConversionServiceImpl(), self.server)
+        self.server.add_insecure_port('[::]:50051')
+
+    def start(self):
+        print('Starting server. Listening on port 50051...')
+        self.server.start()
+
+    def stop(self):
+        print('Stopping server.')
+        self.server.stop(0)
+
 
 class ConversionServiceImpl(units_pb2_grpc.UnitConversionServiceServicer):
+    # def Handler(self, request, context):
+    #     return units_pb2.UnitConversionResponse
+
+    # def ErrorHandler(self, request, context):
+    #     raise RuntimeError('Some Error')
+
     def ConvertUnit(self, request, context):
-        # Delegate the conversion to the units-proto-extensions function
         response = convert_units(request)
 
         return response
 
 
-server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+# server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 
-units_pb2_grpc.add_UnitConversionServiceServicer_to_server(ConversionServiceImpl(), server)
+# units_pb2_grpc.add_UnitConversionServiceServicer_to_server(ConversionServiceImpl(), server)
 
-print('Starting server. Listening on port 50051...')
-server.add_insecure_port('[::]:50051')
-server.start()
+# print('Starting server. Listening on port 50051...')
+# server.add_insecure_port('[::]:50051')
+# server.start()
 
-server.wait_for_termination()
+# if os.environ.get('TEST_ENV') != 'test':
+#     server.wait_for_termination()
 
+
+if __name__ == "__main__":
+    conversion_server = ConversionServer()
+    conversion_server.start()
+    conversion_server.server.wait_for_termination()
