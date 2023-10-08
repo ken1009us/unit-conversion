363938121461dd8d580c483663ca7fd28d831c94
Ken Wu <kenwu1009us@gmail.com>
2023-10-08 11:23:34 -0700

    error and docstring

--- units_core/units.py
+++ units_core/units.py
@@ -1,7 +1,20 @@
-from pint import UnitRegistry
+from pint import UnitRegistry, UndefinedUnitError, DimensionalityError
 
 class Units:
+    """
+    A class used to handle unit conversions.
+
+    Attributes:
+    - unit_registry (UnitRegistry): A registry to handle unit conversions and definitions.
+
+    Methods:
+    - convert(value, from_unit, to_unit): Converts a value from one unit to another.
+    """
+
     def __init__(self):
+        """
+        Initializes the Units class with a unit registry for handling unit conversions.
+        """
         self.unit_registry = UnitRegistry()
 
 
@@ -15,8 +28,20 @@ class Units:
         :return: The converted value.
         """
 
-        quantity = value * self.unit_registry(from_unit)
-        converted_value = quantity.to(to_unit).magnitude
+        try:
+            quantity = value * self.unit_registry(from_unit)
+            converted_value = quantity.to(to_unit).magnitude
 
-        return converted_value
+            return converted_value
 
+        except UndefinedUnitError as e:
+            print(f"Error: The unit '{e.unit}' is not defined.")
+            raise
+
+        except DimensionalityError as e:
+            print(f"Error: Cannot convert from '{from_unit}' to '{to_unit}'. {e}")
+            raise
+
+        except ValueError as e:
+            print(f"Error: The value must be a number. {e}")
+            raise
