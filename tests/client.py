b417203a6595a940f305b213f9e24e17afad1612
Ken Wu <kenwu1009us@gmail.com>
2023-10-08 11:21:59 -0700

    error handling and docstring

--- tests/client.py
+++ tests/client.py
@@ -1,6 +1,6 @@
 import grpc
-import sys
 import os
+import sys
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 myproject_directory = os.path.join(current_directory, '..')
@@ -11,34 +11,73 @@ from units_proto import units_pb2_grpc
 
 
 def run():
-    channel = grpc.insecure_channel('localhost:50051')
-    stub = units_pb2_grpc.UnitConversionServiceStub(channel)
+    """
+    Establishes a connection to the gRPC server and tests unit conversions.
 
-    test_standard_units(stub)
+    Handles any raised exceptions during the connection or conversion processes.
+    """
+    try:
+        channel = grpc.insecure_channel('localhost:50051')
+        stub = units_pb2_grpc.UnitConversionServiceStub(channel)
 
-    test_custom_units(stub)
+        test_standard_units(stub)
+
+        test_custom_units(stub)
+
+    except grpc.RpcError as e:
+        print(f"An error occurred during the RPC call: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 
 def test_standard_units(stub):
-    request = units_pb2.UnitConversionRequest()
-    request.from_unit = 'meter'
-    request.to_unit = 'kilometer'
-    request.value = 1000
+    """
+    Tests the conversion of standard units.
 
-    response = stub.ConvertUnit(request)
+    Parameters:
+    - stub (units_pb2_grpc.UnitConversionServiceStub): The stub to make requests to the gRPC server.
 
-    print('Converted value (standard units): ', response.converted_value)
+    Handles any raised exceptions during the conversion process and prints the converted value.
+    """
+    try:
+        request = units_pb2.UnitConversionRequest()
+        request.from_unit = 'meter'
+        request.to_unit = 'kilometer'
+        request.value = 1000
+
+        response = stub.ConvertUnit(request)
+
+        print('Converted value (standard units): ', response.converted_value)
+
+    except grpc.RpcError as e:
+        print(f"An error occurred during the RPC call: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 
 def test_custom_units(stub):
-    request = units_pb2.UnitConversionRequest()
-    request.from_unit = 'miG'
-    request.to_unit = 'G'
-    request.value = 1024
+    """
+    Tests the conversion of custom units.
 
-    response = stub.ConvertUnit(request)
+    Parameters:
+    - stub (units_pb2_grpc.UnitConversionServiceStub): The stub to make requests to the gRPC server.
 
-    print('Converted value (custom units): ', response.converted_value)
+    Handles any raised exceptions during the conversion process and prints the converted value.
+    """
+    try:
+        request = units_pb2.UnitConversionRequest()
+        request.from_unit = 'miG'
+        request.to_unit = 'G'
+        request.value = 1024
+
+        response = stub.ConvertUnit(request)
+
+        print('Converted value (custom units): ', response.converted_value)
+
+    except grpc.RpcError as e:
+        print(f"An error occurred during the RPC call: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 
 if __name__ == '__main__':
